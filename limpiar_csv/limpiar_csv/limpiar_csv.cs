704b579cafe9b42ebcbbab8ad53b78bbae5e0339
Alexnegre05 <alexnegre62@gmail.com>
2026-03-19 11:00:29 +0100

    arreglo de snl

---
+++ limpiar_csv/limpiar_csv/limpiar_csv.cs
@@ -0,0 +1,10 @@
+﻿namespace limpiar_csv
+{
+    internal class limpiar_csv
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
