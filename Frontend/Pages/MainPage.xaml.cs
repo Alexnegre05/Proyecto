3c80f578c4da00fe9a6ecbaa2aea61218e1f929c
Alexnegre05 <alexnegre62@gmail.com>
2026-03-24 11:44:02 +0100

    mini mejora visual de el frontend

--- Frontend/Pages/MainPage.xaml.cs
+++ Frontend/Pages/MainPage.xaml.cs
@@ -10,5 +10,23 @@ namespace Frontend.Pages
             InitializeComponent();
             BindingContext = model;
         }
+
+        private void OnHoverEnter(object sender, PointerEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                button.BorderColor = Color.FromArgb("#FF6600");
+                button.BorderWidth = 2;
+            }
+        }
+
+        private void OnHoverExit(object sender, PointerEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                button.BorderColor = null;
+                button.BorderWidth = 0;
+            }
+        }
     }
 }
