59d90accb874d5b1b72b592dc53ece6a5fb84666
Alexnegre05 <alexnegre62@gmail.com>
2026-04-07 09:05:22 +0200

    añadida funcion vacia que sirve para que se llame cada vez que se inicie la pagina

--- Frontend/Pages/PonerNotasPage.xaml.cs
+++ Frontend/Pages/PonerNotasPage.xaml.cs
@@ -6,4 +6,19 @@ public partial class PonerNotasPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    // la funcion onAppearing sirve para indicarle a la aplicacion que cuando se abra la pagina  se llame a la funcionq ue quieras
+    protected override void OnAppearing()
+    {
+        base.OnAppearing(); 
+        // esto es para decirle que como estamos sobreescribiendo una pagina que primero ejecute lo que hacia antes la funcion original(con el base)
+
+        EstacionCercana();
+    }
+
+    // funcion para saber el nombre de la estacion mas cercana
+    private void EstacionCercana()
+    {
+
+    }
 }
