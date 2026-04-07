8a6e2b71c22b17bc1de238e7d17adb89aec73d0a
Alexnegre05 <alexnegre62@gmail.com>
2026-04-07 09:23:25 +0200

    que se conecte el frontend con backend

--- Frontend/Pages/PonerNotasPage.xaml.cs
+++ Frontend/Pages/PonerNotasPage.xaml.cs
@@ -1,3 +1,6 @@
+using System.Net.Sockets;
+using System.Net;
+
 namespace Frontend.Pages;
 
 public partial class PonerNotasPage : ContentPage
@@ -7,7 +10,7 @@ public partial class PonerNotasPage : ContentPage
 		InitializeComponent();
 	}
 
-    // la funcion onAppearing sirve para indicarle a la aplicacion que cuando se abra la pagina  se llame a la funcionq ue quieras
+    // la funcion onAppearing sirve para indicarle a la aplicacion que cuando se abra la pagina  se llame a la funcion que quieras
     protected override void OnAppearing()
     {
         base.OnAppearing(); 
@@ -16,9 +19,33 @@ public partial class PonerNotasPage : ContentPage
         EstacionCercana();
     }
 
+    
+
     // funcion para saber el nombre de la estacion mas cercana
-    private void EstacionCercana()
+    // usamos de nuevo el async y el await pero esta vez en la conexion de el socket 
+    private async void EstacionCercana()
     {
 
+        try
+        {
+            // ip que se usa para conectarse en el movil
+            string ip = "192.168.1.50";
+
+            // Creamos el socket 
+            IPAddress address = IPAddress.Parse(ip);  // creamos la ip y el endpoint
+            IPEndPoint endpoint = new IPEndPoint(address, 1000); // el puerto es el 1000
+            Socket frontend_socket = new Socket(address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            await frontend_socket.ConnectAsync(endpoint); // es lo mimso que connect pero preparada para el async 
+
+            Console.WriteLine("Conectado");
+            // cerramos el socket
+            frontend_socket.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+        
     }
 }
