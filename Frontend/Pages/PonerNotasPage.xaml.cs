d06f12bf877ab3490b5dcc8ddc619813285498f0
Alexnegre05 <alexnegre62@gmail.com>
2026-04-10 12:52:59 +0200

    scroll añadido en poner notas

--- Frontend/Pages/PonerNotasPage.xaml.cs
+++ Frontend/Pages/PonerNotasPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net.Sockets;
 using System.Net;
 using System.Text;
+using System.Collections.Generic;
 
 
 
@@ -186,6 +187,60 @@ public partial class PonerNotasPage : ContentPage
             BtnFlecha.Text = LineasView.IsVisible ? "▲" : "▼";
     }
 
+
+    // funcion que se encarga de el main thread
+    private void mainthread(string estacion, List<InfoLinea> paradas)
+    {
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            LabelEstacion.Text = "Estacion: " + estacion;
+            LineasView.ItemsSource = paradas;
+            // lineas view es como el id de collectionview y sirve para poder tenerlo en el backend y
+            // modificar sus atributos igual a label estación que es el nombre de la estacion 
+
+
+            // el selectionchanges es que cuando se cambie la estacion(el selector) que se cambie el nombre
+            // el += no es un a= a + 1 sino aqui
+            // es un añade también esta función a la lista de cosas por hacer cuando ocurra el evento SelectionChanged
+
+            // sender el objeto que disparo el evento y la e es el objeto que tiene los datos sobre el evento
+
+            LineasView.SelectionChanged += (s, e) =>
+            {
+                //  Obtenemos el elemento seleccionado actual, el que es el primero y lo ponemos como dinamico
+                // lo tenemos que pasar con el as a InfoLinea
+
+                InfoLinea seleccion = e.CurrentSelection.FirstOrDefault() as InfoLinea;
+                // dynamic salta la comprovacion de a la hora de crear un objeto 
+
+                if (seleccion != null) // miramos que no sea nulo
+                {
+                    // Cambiamos el texto
+                    LabelEstacion.Text = $"Estación: {estacion} ({seleccion.Nombre})";
+
+
+                    // Usamos el color que viene guardado en el objeto seleccionado
+                    LabelEstacion.TextColor = (Color)seleccion.Color;
+
+                    BordePrincipal.Background = (Color)seleccion.Color;
+
+                    Titulo.TextColor = Colors.White;
+
+                    LineasView.IsVisible = false;
+                    BtnFlecha.Text = "▼";
+
+                    LineasView.SelectedItem = null;
+                    // esto es para que el selector se pueda volver a clicar una segunda vez
+                    // si esta en la version resumida ya que si es la misma letra no detectara el evento 
+                }
+            };
+        });
+    }
+
+
+
+
+
     // funcion para saber el nombre de la estacion mas cercana
     // usamos de nuevo el async y el await pero esta vez en la conexion de el socket 
     private async void EstacionCercana()
@@ -242,55 +297,12 @@ public partial class PonerNotasPage : ContentPage
 
 
             // aqui es donde se cambia el nombre, y todo el tema de el color.
-            
+
             // El MainThread es el que se encarga de dibujar por pantalla
             // le decimos a ese hilo que se invoque y que cambie el texto y que las lineas son las paradas que hemos cogido
 
+            mainthread(estacion, paradas);
 
-            MainThread.BeginInvokeOnMainThread(() =>
-            {
-                LabelEstacion.Text = "Estacion: " + estacion;
-                LineasView.ItemsSource = paradas; 
-                // lineas view es como el id de collectionview y sirve para poder tenerlo en el backend y
-                // modificar sus atributos igual a label estación que es el nombre de la estacion 
-
-
-                // el selectionchanges es que cuando se cambie la estacion(el selector) que se cambie el nombre
-                // el += no es un a= a + 1 sino aqui
-                // es un añade también esta función a la lista de cosas por hacer cuando ocurra el evento SelectionChanged
-
-                // sender el objeto que disparo el evento y la e es el objeto que tiene los datos sobre el evento
-
-                LineasView.SelectionChanged += (s, e) =>
-                {
-                    //  Obtenemos el elemento seleccionado actual, el que es el primero y lo ponemos como dinamico
-                    // lo tenemos que pasar con el as a InfoLinea
-
-                    InfoLinea seleccion = e.CurrentSelection.FirstOrDefault() as InfoLinea;
-                    // dynamic salta la comprovacion de a la hora de crear un objeto 
-
-                    if (seleccion != null) // miramos que no sea nulo
-                    {
-                        // Cambiamos el texto
-                        LabelEstacion.Text = $"Estación: {estacion} ({seleccion.Nombre})";
-
-
-                        // Usamos el color que viene guardado en el objeto seleccionado
-                        LabelEstacion.TextColor = (Color)seleccion.Color;
-
-                        BordePrincipal.Background = (Color)seleccion.Color;
-
-                        Titulo.TextColor = Colors.White;
-
-                        LineasView.IsVisible = false;
-                        BtnFlecha.Text = "▼";
-
-                        LineasView.SelectedItem = null; 
-                        // esto es para que el selector se pueda volver a clicar una segunda vez
-                        // si esta en la version resumida ya que si es la misma letra no detectara el evento 
-                    }
-                };
-            });
 
 
             // cerramos el socket
